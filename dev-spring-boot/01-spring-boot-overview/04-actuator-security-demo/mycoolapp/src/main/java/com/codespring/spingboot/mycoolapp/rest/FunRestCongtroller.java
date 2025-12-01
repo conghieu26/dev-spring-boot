af951cec31d4a0aa73d71bf41e235f30116fc5de
Nguyen Hieu <hieunc00@gmail.com>
2025-12-01 21:08:58 +0700

    feat: integrate Spring Security with basic configuration

---
+++ dev-spring-boot/01-spring-boot-overview/04-actuator-security-demo/mycoolapp/src/main/java/com/codespring/spingboot/mycoolapp/rest/FunRestCongtroller.java
@@ -0,0 +1,32 @@
+package com.codespring.spingboot.mycoolapp.rest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FunRestCongtroller {
+
+    // response / return "Hello World"
+
+    @GetMapping
+    public String GetValue() {
+        return "Hello World1";
+    }
+
+
+    // expose a new endpoint for "workout"
+
+    @GetMapping("/workout")
+    public String getDailyWorkout() {
+        return "Run a hard 5k!";
+    }
+
+    // expose a new endpoint for "fortune"
+
+    @GetMapping("/fortune")
+    public String getDailyFortune() {
+        return "Today is your lucky day.";
+    }
+
+}
+
